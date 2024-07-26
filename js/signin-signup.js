ff35a96d7954be6bc2f3308a08be357d9ed836f5
ikmalif29 <ikmalfauzaeni@gmail.com>
2024-07-26 19:03:51 +0700

    memperbaiki validasi

--- js/signin-signup.js
+++ js/signin-signup.js
@@ -6,36 +6,44 @@ const container = document.getElementById("container");
 // fitur login untuk user,data akan di cek di api
 const buttonLogin = document.querySelector("#button-sign-in");
 buttonLogin.addEventListener("click", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    try {
-        const users = await apiFetch(
-            "https://sepokat-store.vercel.app/api/user/get-all"
-        );
+  try {
+    const users = await apiFetch(
+      "https://sepokat-store.vercel.app/api/user/get-all"
+    );
 
-        const username = document.querySelector("#username").value
-        const password = document.querySelector("#password").value
-        // pengecekan username dan password
-        const user = users.find(
-            (user) => user.username === username && user.pasword === password
-        );
-        // jika username dan password benar makan akan berpindah ke halaman index atau home
-        if (user) {
-            localStorage.setItem("userLogin", JSON.stringify(user))
-            alert("Login successful");
-            window.location.href = "../html/index.html"
-        } else {
-            // jika usename dan pasword salah maka akan menampilkan alert 
-            alert("Gagal login");
-        }
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        document.getElementById("error-message").textContent =
-            "An error occurred. Please try again later.";
+    console.log(users);
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    if (username == "" || password == "") {
+      return alert("Username atau password harus diisi !!!");
     }
+    // pengecekan username dan password
+    const user = users.find(
+      (user) => user.username === username && user.pasword === password
+    );
+    // jika username dan password benar makan akan berpindah ke halaman index atau home
+    if (user) {
+      localStorage.setItem("userLogin", JSON.stringify(user));
+      alert("Login successful");
+      window.location.href = "../html/index.html";
+    } else {
+      // jika usename dan pasword salah maka akan menampilkan alert
+      alert("Gagal login");
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    document.getElementById("error-message").textContent =
+      "An error occurred. Please try again later.";
+  }
 });
 //  sigin atau daftar
-document.getElementById("registerForm").addEventListener("submit", (e) => {
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async (e) => {
     e.preventDefault();
     const dataName = document.querySelector("#input-name").value;
     const dataUsername = document.querySelector("#input-user").value;
@@ -43,30 +51,29 @@ document.getElementById("registerForm").addEventListener("submit", (e) => {
     // data akan di simpan di api
     console.log(dataName);
     let baru = {
-        username: dataUsername,
-        password: dataPass,
-        fullName: dataName,
+      username: dataUsername,
+      password: dataPass,
+      full_name: dataName,
     };
     console.log(baru);
     try {
-        const data = apiFetch(
-            "https://sepokat-store.vercel.app/api/user/register",
-            "POST",
-            baru
-        );
-        console.log("Data fetched successfully:", data);
-        alert(`Sign Up Sucsesfuly`);
-        window.location.reload();
+      const data = await apiFetch(
+        "https://sepokat-store.vercel.app/api/user/register",
+        "POST",
+        baru
+      );
+      console.log("Data fetched successfully:", data);
+      alert(`Sign Up Sucsesfuly`);
+      window.location.reload();
     } catch (error) {
-        console.error("Error fetching data:", error);
+      console.error("Error fetching data:", error);
     }
-})
+  });
 
 signUpButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+  container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+  container.classList.remove("right-panel-active");
 });
-
